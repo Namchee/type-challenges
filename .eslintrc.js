72ad6309eeb7be5d131f2a8ffddb24ee09755dd4
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-04-12 17:23:36 +0700

    chore: integrate tooling

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'xo',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: [
+		'@typescript-eslint',
+    'prettier',
+	],
+	rules: {
+	},
+};
