1134391bc7428fdc67c02312bc18be70deccad48
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-04-16 09:01:20 +0700

    chore: add solution for type-lookup challenge

---
+++ medium/type-lookup.ts
@@ -0,0 +1,25 @@
+/* _____________ Your Code Here _____________ */
+
+type LookUp<U, T extends string> = U extends { type: T } ? U : never;
+
+/* _____________ Test Cases _____________ */
+// DO NOT EDIT
+import { Equal, Expect } from '@type-challenges/utils'
+
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type Animal = Cat | Dog
+
+type cases = [
+  Expect<Equal<LookUp<Animal, 'dog'>, Dog>>,
+  Expect<Equal<LookUp<Animal, 'cat'>, Cat>>,
+]
