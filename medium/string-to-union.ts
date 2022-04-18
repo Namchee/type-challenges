32d245dfb55c567bb0b8f09d58068af4e5f69a17
Cristopher Namchee <cristophernamchee12@gmail.com>
2022-04-18 10:09:10 +0700

    chore: add solution for string to union

---
+++ medium/string-to-union.ts
@@ -0,0 +1,15 @@
+/* _____________ Your Code Here _____________ */
+
+type StringToUnion<T extends string> = T extends `${infer Char}${infer Back}` ? Char | StringToUnion<Back> : never;
+
+
+/* _____________ Test Cases _____________ */
+// DO NOT EDIT
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<StringToUnion<''>, never>>,
+  Expect<Equal<StringToUnion<'t'>, 't'>>,
+  Expect<Equal<StringToUnion<'hello'>, 'h' | 'e' | 'l' | 'l' | 'o'>>,
+  Expect<Equal<StringToUnion<'coronavirus'>, 'c' | 'o' | 'r' | 'o' | 'n' | 'a' | 'v' | 'i' | 'r' | 'u' | 's'>>,
+]
