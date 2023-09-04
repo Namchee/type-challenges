7976e7ffb52f55d70e9c060c4f7ec98e9a183899
Cristopher Namchee <cristophernamchee12@gmail.com>
2023-09-04 22:15:08 +0700

    feat: add solution for kebab-case challenge

---
+++ hard/camel-case.ts
@@ -0,0 +1,21 @@
+/* _____________ Your Code Here _____________ */
+
+type Split<S extends string, SEP extends string | undefined = undefined> = SEP extends undefined ? [S] : S extends `${infer First}${SEP}${infer Second}` ? [First, ...Split<Second, SEP>] : [S];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type A = Split<'Hi! How are you?'>;
+
+type cases = [
+  Expect<Equal<Split<'Hi! How are you?'>, ['Hi! How are you?']>>,
+  Expect<Equal<Split<'Hi! How are you?', 'z'>, ['Hi! How are you?']>>,
+  Expect<Equal<Split<'Hi! How are you?', ' '>, ['Hi!', 'How', 'are', 'you?']>>,
+  Expect<Equal<Split<'Hi! How are you?', ''>, ['H', 'i', '!', ' ', 'H', 'o', 'w', ' ', 'a', 'r', 'e', ' ', 'y', 'o', 'u', '?']>>,
+  Expect<Equal<Split<'', ''>, []>>,
+  Expect<Equal<Split<'The sine in cosine', 'in'>, ['The s', 'e ', ' cos', 'e']>>,
+  Expect<Equal<Split<'Never say never, forever and ever.', 'ver'>, ['Ne', ' say ne', ', fore', ' and e', '.']>>,
+  Expect<Equal<Split<'', 'z'>, ['']>>,
+  Expect<Equal<Split<''>, ['']>>,
+  Expect<Equal<Split<string, 'whatever'>, string[]>>,
+]
