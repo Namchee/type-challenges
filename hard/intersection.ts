c5fc6d2f7335aa85de7575c9128a2fec98d7daf1
Cristopher Namchee <cristophernamchee12@gmail.com>
2023-11-27 14:03:49 +0700

    feat: add solution for intersection challenge

---
+++ hard/intersection.ts
@@ -0,0 +1,19 @@
+/* _____________ Your Code Here _____________ */
+
+type Intersection<T extends unknown[]> = T extends [infer El, ...infer Rest]
+  ? El extends unknown[]
+    ? El[number] & Intersection<Rest>
+    : El & Intersection<Rest>
+  : unknown;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<Intersection<[[1, 2], [2, 3], [2, 2]]>, 2>>,
+  Expect<Equal<Intersection<[[1, 2, 3], [2, 3, 4], [2, 2, 3]]>, 2 | 3>>,
+  Expect<Equal<Intersection<[[1, 2], [3, 4], [5, 6]]>, never>>,
+  Expect<Equal<Intersection<[[1, 2, 3], [2, 3, 4], 3]>, 3>>,
+  Expect<Equal<Intersection<[[1, 2, 3], 2 | 3 | 4, 2 | 3]>, 2 | 3>>,
+  Expect<Equal<Intersection<[[1, 2, 3], 2, 3]>, never>>
+];
